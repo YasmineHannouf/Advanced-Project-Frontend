92ec421fe835797d91c6b4fd2132e104eeb02037
AWIKSARAH <awiksarah@gmail.com>
2023-03-08 14:23:25 +0200

    set

--- advanced-project-frontend/src/App.js
+++ advanced-project-frontend/src/App.js
@@ -1,36 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Incomes from './components/Incomes';
-import RecurringIncome from './components/RecurringIncome';
-import FixedIncomes from './components/FixedIncomes';
-import SignIn from './pages/SignIn';
-import Expenses from './components/Expenses';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Incomes from "./components/Incomes";
+import RecurringIncome from "./components/RecurringIncome";
+import FixedIncomes from "./components/FixedIncomes";
+import SignIn from "./pages/SignIn";
+import Expenses from "./components/Expenses";
+import SideBar from "./components/SideBar";
+import DashHeader from './components/DashHeader';
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Dashboard />}>
-					<Route path="/incomes" element={<Incomes />}>
-						{/* <Route
-							path="/recurring"
-							element={<RecurringIncome />}
-						/> */}
-						{/* <Route path="/fixing" element={<FixedIncomes />} /> */}
-					</Route>
+  return (
+    <BrowserRouter>
+	<DashHeader />
 
-					{/* <Route path="/expenses" element={<Expenses />}>
-						<Route
-							path="/recurring"
-							element={<RecurringIncome />}
-						/>
-						{/* <Route path="/fixing" element={<FixedIncomes />} /> 
-					</Route>*/}
-				</Route> 
-				<Route path="/login" element={<SignIn />} />
-			</Routes>
-		</BrowserRouter>
-	);
+      <SideBar />
+	  
+      <Routes>
+	  <Route path="/login" element={<SignIn />} />
+        <Route path="/" element={<Dashboard />}/>
+          <Route path="/incomes" element={<Incomes />}/>
+            <Route path="/recurring" element={<RecurringIncome />} />
+            <Route path="/fixing" element={<FixedIncomes />} />
+
+          <Route path="/expenses" element={<Expenses />}/>
+            <Route path="/recurring" element={<RecurringIncome />} />
+            <Route path="/fixing" element={<FixedIncomes />} />
+          
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
