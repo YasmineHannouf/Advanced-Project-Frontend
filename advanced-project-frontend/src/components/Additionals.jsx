1173a980ce3ba465cb5340a9c465637795c6ab5d
AWIKSARAH <awiksarah@gmail.com>
2023-03-08 12:35:16 +0200

    Hello

--- advanced-project-frontend/src/components/Additionals.jsx
+++ advanced-project-frontend/src/components/Additionals.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Additionals = () => {
-	return (
-		<div>
-			<h1>hello </h1>
-		</div>
-	);
-};
+export default function DataGridDemo() {
+  const [data, setData] = useState([]);
+  // const [columns, setcolumns] = useState([]);
 
-export default Additionals;
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const [loading, setLoading] = useState(true);
+
+  function fetchData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/todos/';
+    setLoading(true);
+    axios.get(apiUrl)
+      .then((response) => {
+        const data = response.data.map((item) => ({
+          id: item.id,
+          col1: item.title,
+          col2: item.completed ? 'Completed' : 'Incomplete',
+          
+        })); 
+        setData(data); 
+        setLoading(false); 
+      });
+  }
+  const columns = [
+    { field: 'col1', headerName: 'Username', width: 150 },
+        { field: 'col2', headerName: 'Password', width: 150 },
+  ];
+
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
