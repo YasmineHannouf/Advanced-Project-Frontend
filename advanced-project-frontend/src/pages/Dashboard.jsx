595de669c322b24fb96caeeb58af103fc09d4dbc
mahmoudElali <mahmud.elali98@gmail.com>
2023-03-09 09:37:03 +0200

    modified the old sideBar and created a collapsing one

--- advanced-project-frontend/src/pages/Dashboard.jsx
+++ advanced-project-frontend/src/pages/Dashboard.jsx
@@ -8,19 +8,12 @@ import SideBar from '../components/SideBar';
 // styles
 import './../styles/Dashboard.css';
 import '../styles/dashHeader.css';
-import { Route } from 'react-router-dom';
 
 const Dashboard = () => {
-	const [homeExpanded, setHomeExpanded] = useState(true);
-
 	return (
 		<div className="DashboardContainer">
-			
-			<DashHeader />
-			<div style={{ display: 'flex', gridColumn: '1/-1' }}>
-				<SideBar setHomeExpanded={setHomeExpanded} />
-				{/* <Route path="/" element={<DashHome />} /> */}
-			</div>
+			<SideBar />
+			{/* <DashHome />  */}
 		</div>
 	);
 };
