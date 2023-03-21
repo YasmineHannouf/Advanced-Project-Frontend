c19e9972a192b688034ff47bc065d8ea1a2259d6
AWIKSARAH <awiksarah@gmail.com>
2023-03-21 11:11:06 +0200

    Styling login page

--- advanced-project-frontend/src/pages/SignIn.jsx
+++ advanced-project-frontend/src/pages/SignIn.jsx
@@ -2,39 +2,48 @@ import React, { useState } from "react";
 import axios from "axios";
 import Cookie from "universal-cookie";
 import { useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie';
-
+import Cookies from "js-cookie";
+import "../styles/LoginPage.css";
+import image from '../assets/milky-way-2695569_960_720.jpg'
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const navigate = useNavigate();
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     axios
-      .post(`http://localhost:8000/api/login`, {
-        email,
-        password
-      }, {
-        headers: {
-          'Content-Type': 'application/json'
+      .post(
+        `http://localhost:8000/api/login`,
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          }, 
         }
-      })
-      .then(response => {
+      )
+      .then((response) => {
         if (response) {
           // console.log(response.data)
           // const token = response.data.token;
-          const isAuthenticated = Cookies.get('Authorisation');
+          const isAuthenticated = Cookies.get("Authorisation");
 
           const cookie = new Cookie();
-          cookie.set("Authorisation", isAuthenticated, { path: "/", maxAge: 60 * 60 * 24 });
+          cookie.set("Authorisation", isAuthenticated, {
+            path: "/",
+            maxAge: 60 * 60 * 24,
+          });
 
           setSuccess(response);
           alert("you are logged in " + isAuthenticated);
-          navigate("/dash");        }
+          navigate("/dash");
+        }
       })
-      .catch(error => {
+      .catch((error) => {
         if (error) {
           console.log(error);
           setError(error);
@@ -42,15 +51,30 @@ export default function SignIn() {
         alert(error.response.data);
       });
   };
-  
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-        <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-        <button type="submit">Submit</button>
-      </form>
+    <div className="logInContainer">
+      <section  className="imageSection">
+        <img src={image} alt="jijiji" className="image"/>
+      </section>
+      <section className="formSection">
+        {" "}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
       {error && <p>{error}</p>}
     </div>
   );
