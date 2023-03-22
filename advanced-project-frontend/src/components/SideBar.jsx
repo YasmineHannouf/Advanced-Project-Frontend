eee4eef9e8c8412ea461ed313eb0e823670c3a55
AWIKSARAH <awiksarah@gmail.com>
2023-03-22 02:27:09 +0200

    Merge

--- advanced-project-frontend/src/components/SideBar.jsx
+++ advanced-project-frontend/src/components/SideBar.jsx
@@ -2,7 +2,8 @@ import adminImage from '../assets/default_61f3429ad1ced-removebg-preview.png';
 import { useState, useContext } from 'react';
 import React from 'react';
 import '../styles/sideBar.css';
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -15,43 +16,66 @@ import { context, Context } from '../App.js';
 
 library.add(fas);
 
-const sideBarLinks = [
-	{
-		name: 'home',
-		path: '/',
-		icon: <FontAwesomeIcon icon="fa-solid fa-house" />,
-	},
-	{
-		name: 'Expenses',
-		path: '/expenses',
-		icon: <FontAwesomeIcon icon="fa-solid fa-money-bill-transfer" />,
-	},
-	{
-		name: 'Incomes',
-		path: '/incomes',
-		icon: <FontAwesomeIcon icon="fa-solid fa-download" />,
-	},
+const SideBar = (props) => {
 
-	{
-		name: 'Settings',
-		path: 'settings',
-		icon: <FontAwesomeIcon icon="fa-solid fa-gear" />,
-	},
-	{
-		name: 'Additionals',
-		path: '/categories',
-		icon: <FontAwesomeIcon icon="fa-solid fa-grip" />,
-	},
-	{
-		name: 'Goals Tracker',
-		path: 'goals',
-		icon: <FontAwesomeIcon icon="fa-solid fa-bullseye" />,
-	},
-];
+  const navigate = useNavigate();
+  const [status, setStatus] = useState(props.status);
+  const handleLogOut = () => {
+    axios
+      .get("http://127.0.0.1:8000/api/logout")
+      .then((response) => {
+        console.log(response);
+        navigate("/");
+      })
+      .then((err) => {
+        console.log(err);
+      });
+  };
+  const sideBarLinks = [
+    {
+      name: "home",
+      path: "/home",
+      icon: <FontAwesomeIcon icon="fa-solid fa-house" />,
+    },
+    {
+      name: "Expenses",
+      path: "/expenses",
+      icon: <FontAwesomeIcon icon="fa-solid fa-money-bill-transfer" />,
+    },
+    {
+      name: "Incomes",
+      path: "/incomes",
+      icon: <FontAwesomeIcon icon="fa-solid fa-download" />,
+    },
 
-const SideBar = () => {
+    {
+      name: "Additionals",
+      icon: <FontAwesomeIcon icon="fa-solid fa-grip" />,
+      subLinks: [
+        {
+          name: "Category",
+          path: "/category",
+        },
+        {
+          name: "Fixed Key",
+          path: "/fixedkey",
+        },
+      ],
+    },
+
+    status && {
+      name: "ManageAdmin",
+      path: "/manageadmin",
+      icon: <FontAwesomeIcon icon="fa-solid fa-gear" />,
+    },
+
+    {
+      name: "Goals Tracker",
+      path: "/goals",
+      icon: <FontAwesomeIcon icon="fa-solid fa-bullseye" />,
+    },
+  ];
 	const [sideBarExpanded, setsideBarExpanded] = useContext(context);
-	// const [sideExpanded, setSideExpanded] = useState(context);
 	return (
 		<aside className={sideBarExpanded ? 'sideBar' : 'sideBar_expanded'}>
 			<div className="brandContainer">
@@ -103,7 +127,7 @@ const SideBar = () => {
 					<img src={adminImage} alt="admin" />
 				</div>
 				<div>
-					<h4>dev name </h4>
+					<h4>{props.name}  </h4>
 					<p>Administrator</p>
 				</div>
 				<FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" />
